7510312f728a9c4ea66f697d5ba9e0647cd8b6cd
Anthony <yefremov.anthony@gmail.com>
2023-02-26 22:41:43 +0600

    refactor: games moved to data folder

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
     <div className="header">
       <div className="wrapper">
         <Link to="/" className="header__store-title">
-          Game&nbsp;Store
+          Online{" "}Store
         </Link>
       </div>
       <div className="wrapper header__cart-btn-wrapper">
--- src/pages/order-page/order-page.jsx
+++ src/pages/order-page/order-page.jsx
@@ -6,53 +6,32 @@ import "./order-page.css";
 
 export const OrderPage = () => {
   const items = useSelector((state) => state.cart.itemsInCart);
-  const counter = Math.abs(items.length) % 100;
-  var counterNext = counter % 10;
-  if (counterNext === 1) {
-    return (
-      <div className="order-page">
-        <div className="order-page__left">
-          {items.map((order, index) => (
-            <OrderItem key={`order-${index}`} game={order} />
-          ))}
-        </div>
-        <div className="order-page__right">
-          <div className="order-page__total-price">
-            <span className="order-page__total-price-block">
-              Purchase{" "}
-              <span className="order-page__price-select">{items.length}</span>{" "}
-              item for{" "}
-              <span className="order-page__price-select">
-                $&nbsp;{calcTotalPrice(items)}.
-              </span>
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  if (items.length < 1) {
-    return <h2 className="order-page__title-empty">Your cart is empty</h2>;
-  }
+
   return (
     <div className="order-page">
-      <div className="order-page__left">
-        {items.map((order, index) => (
-          <OrderItem key={`order-${index}`} game={order} />
-        ))}
-      </div>
-      <div className="order-page__right">
-        <div className="order-page__total-price">
-          <span className="order-page__total-price-block">
-            Purchase{" "}
-            <span className="order-page__price-select">{items.length}</span>{" "}
-            items for{" "}
-            <span className="order-page__price-select">
-              $ {calcTotalPrice(items)}.
-            </span>
-          </span>
-        </div>
-      </div>
+      {items.length > 0 ? (
+        <>
+          <div className="order-page__left">
+            {items.map((order, index) => (
+              <OrderItem key={`order-${index}`} game={order} />
+            ))}
+          </div>
+          <div className="order-page__right">
+            <div className="order-page__total-price">
+              <span className="order-page__total-price-block">
+                Purchase{" "}
+                <span className="order-page__price-select">{items.length}</span>{" "}
+                item{items.length > 1 ? "s" : ""}{" "}for{" "}
+                <span className="order-page__price-select">
+                  ${" "}{calcTotalPrice(items)}.
+                </span>
+              </span>
+            </div>
+          </div>
+        </>
+      ) : (
+        <h2 className="order-page__title-empty">Your cart is empty</h2>
+      )}
     </div>
   );
 };
